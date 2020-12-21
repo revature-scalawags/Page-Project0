5ac481db07fd1f7413a515731661ce04df481b00
My Lord <page.c.tyler@gmail.com>
2020-12-21 12:56:57 -0500

    completed funtion thata returns the usage string setup as help feature for cli args.

--- src/main/scala/KeplerDataReader/KeplerDR.scala
+++ src/main/scala/KeplerDataReader/KeplerDR.scala
@@ -1,7 +1,5 @@
 package KeplerDataReader
 
-import java.io.{BufferedWriter, File, FileWriter}
-
 import org.mongodb.scala.MongoClient
 
 import scala.collection.mutable.ArrayBuffer
@@ -13,31 +11,32 @@ import scala.concurrent.ExecutionContext.Implicits.global
  *
  */
 object KeplerDR extends App {
-  val terminal = new UI
-  terminal.welcomeMessage()
+  val ui = new UI
+  ui.welcomeMessage()
 
   val bufferedSource = io.Source.fromFile("cumulative_kepler2020.csv")
   val csv = CSVHandler()
   val planets = csv.getAllPlanets(bufferedSource)
   bufferedSource.close
-  terminal.logger("Done.")
+  ui.logger("Done.")
 
   if (args.length == 0) {
-    terminal.logger("No arguments found. Run with \"-help\" to see usage.")
+    ui.logger("No arguments found. Run with \"-help\" to see usage.")
     System.exit(0)
   }
 
   val colNumbers = ArrayBuffer[Int]()
-  val columns = args.filter(_.startsWith("--"))
+  val columns = args.filter(_.startsWith("-"))
   columns.foreach {
-    case "--yr" => colNumbers += 6
-    case "--op" => colNumbers += 11
-    case "--r" => colNumbers += 13
-    case "--m" => colNumbers += 15
-    case "--t" => colNumbers += 20
-    case "--d" => colNumbers += 35
-    case "--sm" => colNumbers += 26
-    case "--sr" => colNumbers += 25
+    case "-yr" => colNumbers += 6
+    case "-op" => colNumbers += 11
+    case "-r" => colNumbers += 13
+    case "-m" => colNumbers += 15
+    case "-t" => colNumbers += 20
+    case "-d" => colNumbers += 35
+    case "-sm" => colNumbers += 26
+    case "-sr" => colNumbers += 25
+    case "-help" | "-h" => ui.usage()
     case col => println(s"$col is not recognized.\n use -help to see usage.")
   }
   val table = csv.buildNewCSVTable(planets, colNumbers.toArray)
--- src/main/scala/KeplerDataReader/UI.scala
+++ src/main/scala/KeplerDataReader/UI.scala
@@ -11,6 +11,39 @@ case class UI() {
     println(message)
   }
 
+  def usage(): Unit = {
+    println(
+      """
+        |Imports the NASA provided data file for the exoplanets found with the Kepler project.
+        |
+        |The user provides command line arguments to select whichever columns from the data source they'd like
+        |  to have in their generated .csv file and loaded to a database.
+        |
+        |* Note: All generated tables will at least include columns for the planet names and its host star name.
+        |
+        |Example (sbt):
+        |  run -t -m -d -r
+        |
+        |Options:
+        |  -help, -h                             render aid
+        |
+        |  -Columns:
+        |   -yr                                   Discovery Year
+        |   -op                                   Orbital Period
+        |   -m                                    Planetary Mass (earth mass)
+        |   -r                                    Radius (earth radius)
+        |   -t                                    Equilibrium Temperature (Kelvin)
+        |   -d                                    Distance (parsecs) i.e. 1pc = 3.26 light years
+        |   -sm                                   Stellar Mass (solar mass)
+        |   -sr                                   Stellar Radius (solar radius)
+        |
+        |   --a                                   Generate a table with all possible columns.
+        |
+        |
+              """.stripMargin)
+    System.exit(0)
+  }
+
 /*  def promptForColumns(column: String): Boolean = {
 
   }*/
